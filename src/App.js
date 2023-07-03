b8ff8934569ffab43ae679118b803d99e531a0a1
aydinonac <aydinonac@yahoo.com>
2023-07-03 14:08:01 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter";
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import React from 'react';
+
+function refreshPage() { 
+  window.location.reload(false);
+}
+
+function Counter() {
+  const [product, setProduct] = useState({
+    name: 'My Fruits order',
+    count: 0,
+    discount: 0,
+    col: 'pink',
+  });
+  
+  const handlerPlus = () => {
+    setProduct(prev => {
+      if (prev.count >= 5) {
+         prev.discount = 'Special 10'
+         prev.col = 'yellow'
+      };
+      if (prev.count >= 10) {
+          prev.discount = 'Star customer: 20'
+          prev.col = '#ff66ff'
+      };
+      return {
+        ...product,
+        count:prev.count + 1,        
+      };
+      
+    })
+  }
+
+  const handlerMinus = () => {
+    setProduct(prev => {
+      if (prev.count <= 11) {
+          prev.discount = 'Special 10'
+          prev.col = 'yellow'
+      };
+      if (prev.count <= 6) {
+          prev.discount = 0
+          prev.col = 'pink'
+      };
+      if (prev.count < 1) {
+          prev.name = 'Cannot be negative! Please try again'
+          setTimeout(() => {refreshPage()}, 1500);
+      }
+      return {
+        ...product,
+        count:prev.count - 1
+      };
+    })
+  }
+
+    if (product.count < 0) {
+      return (
+        <h2 style={{color: 'red', background: '#eeeeee', padding: 20}}>{product.name}</h2>
+      )
+    }
+
+    return (
+    <>
+      <h2 style={{
+         background: '#eee', padding: 10,
+         marginRight: 150, marginLeft: 150}}>
+         {product.name}</h2>
+      <button onClick={handlerMinus}>-</button>
+      <span >{product.count}</span>
+      <button onClick={handlerPlus}>+</button>
+      <button style={{
+        width: 300, fontSize: 20, background: product.col
+      }}>{product.discount} % discount</button>
+      <br/>
+      <button style={{
+        width: 200, fontSize: 20, background: '#eee'}} 
+        onClick={refreshPage}>Click to restart!</button>
+    </>
+  );
+}
+export default Counter;
